b8b907be1d917ed3041a4de2144dbb8f450cbc64
jakerg@byu.edu <jakerg@byu.edu>
2024-03-15 13:41:56 -0600

    Final Commits

--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -7,7 +7,7 @@ function Header() {
         <img src={logo} className="logo" alt="logo" width={200} />
       </div>
       <div className="col subtitle">
-        <h1 className="text-white">All Bowlers Welcome</h1>
+        <h1 className="text-white">Bowler Info Page</h1>
       </div>
     </header>
   );
